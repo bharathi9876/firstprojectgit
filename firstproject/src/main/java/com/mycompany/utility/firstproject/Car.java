abe946350c3187767649a8625c9cab6e429a2522
bharathi9876 <116981690+bharathi9876@users.noreply.github.com>
2022-11-11 21:00:25 -0500

    initial commit

---
+++ firstproject/src/main/java/com/mycompany/utility/firstproject/Car.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.utility.firstproject;
+
+/**
+ *
+ * @author giril
+ */
+public class Car {
+    private String name;
+    private int price;
+
+    public Car() {
+    }
+
+    public Car(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "name=" + name + ", price=" + price + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+}
---
+++ firstproject/src/test/java/CarTest.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+import com.mycompany.utility.firstproject.Car;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author giril
+ */
+public class CarTest {
+    
+    Car c;
+    
+    public CarTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+       
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+         c = new Car();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    
+    @Test
+    public void testName(){
+        c.setName("Audi");
+        assertEquals("Audi", c.getName());
+    }
+    
+}
